296334961c6f545461ea76f659c7745f86547c02
szahler <zahlersebastian@gmail.com>
2020-12-16 09:53:50 -0800

    Add files via upload

---
+++ Acquisition/_Hardware/Arduino/strain_gauge/strain_gauge.ino
@@ -0,0 +1,35 @@
+#include <Q2HX711.h>
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+
+const byte hx711_data_pin_L = 4;
+const byte hx711_clock_pin_L = 5;
+const byte hx711_data_pin_R = 7;
+const byte hx711_clock_pin_R = 8;
+int val_L = 0;
+int val_R = 0;
+int x = 0;
+int reset = 0;
+Q2HX711 hx711_L(hx711_data_pin_L, hx711_clock_pin_L);
+Q2HX711 hx711_R(hx711_data_pin_R, hx711_clock_pin_R);
+
+Adafruit_MCP4725 dac;
+void setup() {
+  Serial.begin(9600);
+  dac.begin(0x62);
+  val_L = hx711_L.read()/100;
+  val_R = hx711_R.read()/100;
+}
+void loop() {
+  if (Serial.available() > 0) {
+    reset = Serial.read();
+    if (reset == '1') {
+        x = 2045 + val_R - val_L;
+        reset = 0;
+    }
+  }
+  val_L = hx711_L.read()/250;
+  val_R = hx711_R.read()/250;
+  Serial.println(val_L - val_R + x);
+  dac.setVoltage(val_L - val_R + x, false);
+}
